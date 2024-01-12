2f6bb1b99e200b5d786fd022a027631ae761cb0e
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2024-01-13 01:40:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (75.57%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1281-subtract-the-product-and-sum-of-digits-of-an-integer/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        
+        int prod = 1;
+        int sum = 0;
+        while(n!=0)
+        {
+            int r = n%10;
+             prod = prod*r;
+             sum = sum+r;
+             n=n/10;
+        }
+        int result = prod - sum;
+        
+
+        return result;
+        
+    }
+};
