6f66fa101e9d9674dd7a8b3b0a08050e1d0ec634
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2024-09-21 22:38:30 +0530

    Time: 0 ms (100%), Space: 10.3 MB (25.97%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // int left = 0;
+        // int right = nums.size()-1;
+
+        // for(int i = 0;i<nums.size();i++)
+        // {
+
+        // }
+        sort(nums.begin(),nums.end());
+    }
+};
