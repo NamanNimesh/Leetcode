6328e814c3a905c78a3b8b8b68399f28068c4233
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2025-12-26 23:09:20 +0530

    Time: 0 ms (100%), Space: 11.8 MB (9.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,6 +4,7 @@ public:
         int k = 0;
 
         for (int i = 0; i < nums.size(); i++) {
+            // here we are checking for elemment val in nums if it doesnt equal to array then we copy into the kth positon which is staring from zero in nums array 
             if (nums[i] != val) {
                 nums[k] = nums[i];
                 k++;
