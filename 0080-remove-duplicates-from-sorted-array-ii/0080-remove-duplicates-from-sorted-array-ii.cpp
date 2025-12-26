c27e280e6ebb910a6856694448c36bdc88b421ea
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2025-12-27 00:18:45 +0530

    Time: 10 ms (38.77%), Space: 19.6 MB (56.73%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+    // What we are doing is that starting at index 2 because there is always 2 elements present in the array and comparing with the -2 index for removing any type of buffer overflow
        if(nums.size()<=2) return nums.size();
        int k = 2;
        for(int i=2; i< nums.size();i++){
