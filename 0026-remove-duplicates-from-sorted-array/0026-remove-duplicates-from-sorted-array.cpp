473fa80f120d729eff9661fab0a616705bd35ee1
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2025-12-26 23:27:32 +0530

    Time: 0 ms (100%), Space: 22.6 MB (49.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k =0;
+        if(nums.size() == 0){
+            return 0;
+        }
+        //here is a small edge case of buffer overflow so you should start with 1 because first element is always unique
+        int k =1;
         for(int i=1; i< nums.size();i++){
-           if(nums[i] != nums[i+1]){
+           if(nums[i] != nums[i-1]){
             nums[k]= nums[i];
             k++;
            }
