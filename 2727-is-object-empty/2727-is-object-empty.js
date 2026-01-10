6259d9d3d8f6ae2027ab6a049eb0d2a5131ab784
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2026-01-10 18:51:27 +0530

    Time: 46 ms (51.7%), Space: 54 MB (81.65%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(obj && Object.keys(obj).length === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+};
