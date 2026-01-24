485a3eb1546feaf29a001b05c842bbd0c353b221
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2026-01-24 21:49:08 +0530

    Time: 31 ms (30.05%), Space: 16.8 MB (58.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,33 @@
 class Solution {
 private:
-    void dfs(int i, int j, vector<vector<char>>& vis,
-             vector<vector<char>>& grid) {
+    void dfs(int i, int j, vector<vector<char>>& vis, vector<vector<char>>& grid){
         int row = grid.size();
         int col = grid[0].size();
-
-        // FIRST: boundary + stop checks
-        if (i < 0 || j < 0 || i >= row || j >= col || grid[i][j] == '0' ||
-            vis[i][j] == '1') {
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j] == '0' || vis[i][j] == '1'){
             return;
-        }
-
-        vis[i][j] = '1'; // mark visited AFTER checks
-
-        dfs(i - 1, j, vis, grid); // up
-        dfs(i + 1, j, vis, grid); // down
-        dfs(i, j - 1, vis, grid); // left
-        dfs(i, j + 1, vis, grid); // right
+        }        
+        vis[i][j] = '1';
+        dfs(i-1,j,vis,grid);
+        dfs(i+1,j,vis,grid);
+        dfs(i,j-1,vis,grid);
+        dfs(i,j+1,vis,grid);
     }
 
 public:
     int numIslands(vector<vector<char>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-
-        vector<vector<char>> vis(row, vector<char>(col, '0'));
         int islands = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == '1' && vis[i][j] == '0') {
+        vector<vector<char>> vis(row, vector<char>(col, '0'));
+        for(int i = 0; i< row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == '1' && vis[i][j] == '0'){
                     islands++;
-                    dfs(i, j, vis, grid);
+                    dfs(i,j,vis,grid);
                 }
             }
         }
         return islands;
+        
     }
 };
